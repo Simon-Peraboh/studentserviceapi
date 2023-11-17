9bfe45dbb5be4094ab50ff8d54a3879815641f26
Simon-Peraboh Charity <teidei@live.com>
2023-11-17 04:35:27 +0100

    Initial commit

---
+++ studentserviceapi/src/main/java/spc/com/studentserviceapi/StudentserviceapiApplication.java
@@ -0,0 +1,13 @@
+package spc.com.studentserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentserviceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentserviceapiApplication.class, args);
+	}
+
+}
